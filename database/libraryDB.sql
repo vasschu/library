2fc7e38de01c066a1f5059e311edf5510528275b
Virzhinia Petkova <petkovavirzhinia@gmail.com>
2020-09-30 19:12:20 +0300

    database schema

--- database/libraryDB.sql
+++ database/libraryDB.sql
@@ -16,6 +16,62 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `banned_users`
+--
+
+DROP TABLE IF EXISTS `banned_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `banned_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `reason` varchar(45) DEFAULT NULL,
+  `expire_date` date DEFAULT NULL,
+  `users_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_banned_users_users1_idx` (`users_id`),
+  CONSTRAINT `fk_banned_users_users1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `banned_users`
+--
+
+LOCK TABLES `banned_users` WRITE;
+/*!40000 ALTER TABLE `banned_users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `banned_users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `book_ratings`
+--
+
+DROP TABLE IF EXISTS `book_ratings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `book_ratings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `rating` int(11) DEFAULT NULL,
+  `users_id` int(11) NOT NULL,
+  `books_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`,`users_id`,`books_id`),
+  KEY `fk_book_ratings_users1_idx` (`users_id`),
+  KEY `fk_book_ratings_books1_idx` (`books_id`),
+  CONSTRAINT `fk_book_ratings_books1` FOREIGN KEY (`books_id`) REFERENCES `books` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_book_ratings_users1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `book_ratings`
+--
+
+LOCK TABLES `book_ratings` WRITE;
+/*!40000 ALTER TABLE `book_ratings` DISABLE KEYS */;
+/*!40000 ALTER TABLE `book_ratings` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `books`
 --
 
@@ -39,6 +95,116 @@ LOCK TABLES `books` WRITE;
 /*!40000 ALTER TABLE `books` DISABLE KEYS */;
 INSERT INTO `books` VALUES (1,'Harry Potter','JKR','Harry is a wizard'),(2,'Harry Potter 2','JKR','Harry meets young Voldemort'),(3,'The Hunger Games','Suzzane Collins','Katniss goes to reality show war'),(4,'YDKJS','Kyle',NULL);
 /*!40000 ALTER TABLE `books` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `borrowed_books`
+--
+
+DROP TABLE IF EXISTS `borrowed_books`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `borrowed_books` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `users_id` int(11) NOT NULL,
+  `books_id` int(11) NOT NULL,
+  `borrow_date` date NOT NULL,
+  `return_date` date DEFAULT NULL,
+  `isDeleted` tinyint(3) unsigned zerofill NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_borrowed_books_users1_idx` (`users_id`),
+  KEY `fk_borrowed_books_books1_idx` (`books_id`),
+  CONSTRAINT `fk_borrowed_books_books1` FOREIGN KEY (`books_id`) REFERENCES `books` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_borrowed_books_users1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `borrowed_books`
+--
+
+LOCK TABLES `borrowed_books` WRITE;
+/*!40000 ALTER TABLE `borrowed_books` DISABLE KEYS */;
+/*!40000 ALTER TABLE `borrowed_books` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `review_likes`
+--
+
+DROP TABLE IF EXISTS `review_likes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `review_likes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `rating` int(11) DEFAULT NULL,
+  `reviews_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`,`reviews_id`),
+  KEY `fk_review_likes_reviews1_idx` (`reviews_id`),
+  CONSTRAINT `fk_review_likes_reviews1` FOREIGN KEY (`reviews_id`) REFERENCES `reviews` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `review_likes`
+--
+
+LOCK TABLES `review_likes` WRITE;
+/*!40000 ALTER TABLE `review_likes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `review_likes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reviews`
+--
+
+DROP TABLE IF EXISTS `reviews`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `reviews` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) NOT NULL,
+  `content` varchar(45) NOT NULL,
+  `users_id` int(11) NOT NULL,
+  `books_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`,`users_id`,`books_id`),
+  KEY `fk_reviews_users_idx` (`users_id`),
+  KEY `fk_reviews_books1_idx` (`books_id`),
+  CONSTRAINT `fk_reviews_books1` FOREIGN KEY (`books_id`) REFERENCES `books` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_reviews_users` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reviews`
+--
+
+LOCK TABLES `reviews` WRITE;
+/*!40000 ALTER TABLE `reviews` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reviews` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_levels`
+--
+
+DROP TABLE IF EXISTS `user_levels`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_levels` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `level` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_levels`
+--
+
+LOCK TABLES `user_levels` WRITE;
+/*!40000 ALTER TABLE `user_levels` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_levels` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -84,4 +250,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-09-29 22:37:03
+-- Dump completed on 2020-09-30 19:08:42
