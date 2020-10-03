e9961cac9ecae0d7360c3bd94e354f8756f07292
Virzhinia Petkova <petkovavirzhinia@gmail.com>
2020-10-03 08:52:38 +0300

    updating validations again

--- common/error-messages/review-errors.js
+++ common/error-messages/review-errors.js
@@ -1,7 +1,6 @@
 export default {
-    NO_SUCH_REVIEW: 'There is no such review',
-    OTHER_USER_REVIEW: 'You can not edit other users\' reviews',
-    NO_DATABASE_CHANGES: 'There were no changes in the database',
-    NO_SUCH_BOOK: 'There is no such book',
-    NO_REVIEWS_FOR_BOOK: 'There are no reviews for this book',
+    NOT_FOUND: 1,
+    NOT_PERMITTED: 2,
+    NO_DATABASE_CHANGES: 3,
+    DUPLICATE_RECORD: 4,
 };
