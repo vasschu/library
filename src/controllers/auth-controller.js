3d9c014df5af745ce9916b382cc2b7dda9cc7714
Virzhinia Petkova <petkovavirzhinia@gmail.com>
2020-10-04 20:40:58 +0300

    adding login body schema validation

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -3,10 +3,13 @@ import serviceErrors from '../common/error-messages/service-errors.js';
 import usersService from '../service/users-service.js';
 import {createToken} from '../auth/create-token.js';
 
+import {logInBody} from '../validators/login-body.js';
+import {validateBody} from '../middleware/body-validator.js';
+
 const authController = express.Router();
 
 authController
-.post('/login', async (req, res) => {
+.post('/login', validateBody(logInBody), async (req, res) => {
     const body = req.body;
 
     const user = await usersService.logIn(body);
