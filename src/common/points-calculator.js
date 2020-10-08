318f4af948175032f67f150ff57c847894f9f98c
Virzhinia Petkova <petkovavirzhinia@gmail.com>
2020-10-09 00:30:09 +0300

    minor change in changeLevel()

--- src/common/points-calculator.js
+++ src/common/points-calculator.js
@@ -65,7 +65,7 @@ export const calculatePoints = async (userId) => {
 export const changeLevel = async (userId, role) => {
 	const points = await calculatePoints(userId);
 
-		let changeLevel;
+		let changeLevel = role;
 		if (points <= userLevels.powerReader && (role !== 'regular' && role !== 'admin')) {
 			changeLevel = await libraryData.changeLevel(userId, 'regular');
 
