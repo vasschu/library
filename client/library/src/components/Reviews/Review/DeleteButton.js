2599e781db1befa34c2d05407da20964714e10af
Vassil Christov <vassil.christov@gmail.com>
2020-10-20 11:53:21 +0300

    add buttons for reviews

---
+++ client/library/src/components/Reviews/Review/DeleteButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DeleteButton = (id) => {
+	const deleteButton = <button className='delete-review-button'>Delete</button>;
+
+	return <>{deleteButton}</>;
+};
+
+export default DeleteButton;
--- client/library/src/components/Reviews/Review/Review.js
+++ client/library/src/components/Reviews/Review/Review.js
@@ -1,17 +1,24 @@
 import React, { useState, useEffect } from 'react';
+import DeleteButton from './DeleteButton';
+import EditButton from './EditButton';
 
 const Review = (props) => {
 	const [review, setReview] = useState([]);
 	const [error, setError] = useState(null);
 
+	//must create logic to edit review
+	//must create logic to dislay edit and delete button only when needed.
+
 	return (
-  <div className='review'>
-    <hr />
-    <h5>Title: {props.review.title}</h5>
-    <hr />
-    <p>Review:{props.review.content}</p>
-    <p>From: {props.review.username}</p>
-  </div>
+		<div className='review'>
+			<hr />
+			<h5>Title: {props.review.title}</h5>
+			<hr />
+			<p>Review:{props.review.content}</p>
+			<p>From: {props.review.username}</p>
+			<DeleteButton id={props.review.id} />
+			<EditButton id={props.review.id} />
+		</div>
 	);
 };
 
