031b99c95740d16fb5cd1bee71e4369cfcd7f8db
Vassil Christov <vassil.christov@gmail.com>
2020-10-22 16:38:10 +0300

    - login - server endpoint consumed.

--- client/library/src/App.js
+++ client/library/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
@@ -7,23 +7,32 @@ import Books from './components/Books/Books';
 import User from './components/User/User';
 import NotFound from './components/NotFound';
 import LandingPage from './components/LandingPage/LandingPage';
+import LoginPage from './components/LoginPage/LoginPage';
 import IndividualBook from './components/IndividualBook/IndividualBook';
+import { AuthContext } from './components/Context/AuthContext';
 
 function App() {
+	const [authValue, setAuthValue] = useState(false);
+
 	return (
   <div className='App'>
     <BrowserRouter>
-      <Header />
-      <Switch>
-        {/* We can add conditional for redirect auth user -> homepage / unauth user -> landingpage */}
-        <Redirect path='/' exact to='/landing' />
-        <Route path='/landing' component={LandingPage} />
-        <Route path='/books' exact component={Books} />
-        <Route path='/books/:id' component={IndividualBook} />
-        <Route path='/user' exact component={User} />
-        <Route path='*' component={NotFound} />
-      </Switch>
-      <Footer />
+      <AuthContext.Provider
+        value={{ isLoggedIn: authValue, setLoginState: setAuthValue }}
+      >
+        <Header />
+        <Switch>
+          {/* We can add conditional for redirect auth user -> homepage / unauth user -> landingpage */}
+          <Redirect path='/' exact to='/landing' />
+          <Route path='/landing' component={LandingPage} />
+          <Route path='/login' component={LoginPage} />
+          <Route path='/books' exact component={Books} />
+          <Route path='/books/:id' component={IndividualBook} />
+          <Route path='/user' exact component={User} />
+          <Route path='*' component={NotFound} />
+        </Switch>
+        <Footer />
+      </AuthContext.Provider>
     </BrowserRouter>
   </div>
 	);
---
+++ client/library/src/components/LoginPage/LoginPage.js
@@ -0,0 +1,59 @@
+/* eslint-disable react/prop-types */
+import React, { useEffect, useState } from 'react';
+
+const LoginPage = () => {
+	const [user, onChangeUser] = useState('');
+  const [pass, onChangePass] = useState('');
+  
+  const data ={ username: user,
+  password: pass}
+
+  const login = () => {
+
+    fetch(`http://localhost:5500/auth/session`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((res) => {if (typeof res.token === 'string' && res.token.length > 20){
+        console.log('token recieved');
+        console.log(res.token);
+      }})
+      .catch((err) => console.log(err))
+  }
+  
+  const updateUser = (value) => onChangeUser(value)
+	const updatePass = (value) => onChangePass(value)
+
+	return (
+  <div className='login-page-layout'>
+    <div className='call-to-action'>
+      <h1 className='call-to-action-text'>
+        Login to get access to the world's greatest stories
+      </h1>
+      <br />
+      <span>User</span>
+      <input
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        onChange={(e) => updateUser(e.target.value)}
+        value={user}
+      />
+      <br />
+      <span>Pass</span>
+      <input
+      type='password'
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        onChange={(e) => updatePass(e.target.value)}
+        value={pass}
+      />
+      <br />
+      <button className='login-button' onClick={login}>
+			Login
+		</button>
+    </div>
+  </div>
+	);
+};
+
+export default LoginPage;
