b7de7c957757bb419265e5d762b08f532a7dc000
Vassil Christov <vassil.christov@gmail.com>
2020-10-23 10:29:03 +0300

    token extractor from local storage

--- client/library/src/common/common.js
+++ client/library/src/common/common.js
@@ -1,1 +1,13 @@
-//insert common functions here
+//get token from local storage
+const token = localStorage.getItem('token');
+
+const parseJwt = (token) => {
+	if (!token) {
+		return;
+	}
+	const base64Url = token.split('.')[1];
+	const base64 = base64Url.replace('-', '+').replace('_', '/');
+	return JSON.parse(window.atob(base64));
+};
+
+export const tokenData = parseJwt(token);
