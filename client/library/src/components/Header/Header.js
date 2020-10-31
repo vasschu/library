3dccf96b113b686d142851fa696cb0f4e1ea947e
Vassil Christov <vassil.christov@gmail.com>
2020-10-31 17:09:22 +0200

    close #80

--- client/library/src/components/Header/Header.js
+++ client/library/src/components/Header/Header.js
@@ -4,7 +4,7 @@ import { AuthContext } from '../../context/AuthContext';
 import './Header.css';
 import userData from './../../data/userData.js';
 import { useHistory } from 'react-router-dom';
-
+import { toastSuccess, toastError } from './../../common/toaster';
 
 const Header = () => {
 	const style = {
@@ -12,54 +12,59 @@ const Header = () => {
 		textDecoration: 'none',
 		color: 'black',
 	};
-  const { isLoggedIn, setLoginState } = useContext(AuthContext);
-  
-  let history = useHistory();
+	const { isLoggedIn, setLoginState } = useContext(AuthContext);
+
+	let history = useHistory();
 
 	const logout = () => {
-    userData.logoutUser()
-    .then((res) => alert(res.data.message))
-    setLoginState(false);
-    history.push('/landing')
+		userData
+			.logoutUser()
+			.then((res) => {
+				console.log(res);
+				toastSuccess(res.data.message);
+				setLoginState(false);
+				history.push('/landing');
+			})
+			.catch((err) => toastError(err));
 	};
 
 	// this should check if user is authenticated - but we need to make auth first :)
 	// use 'auth' === 'auth' on line 10 for auth test version
 	const header_option = !isLoggedIn ? (
-  <div className='unauth'>
-    <NavLink to='/landing' style={style}>
-      <h1>Library</h1>
-    </NavLink>
-    <div className='auth-buttons'>
-      <NavLink to='/register'>
-        <button className='register'>Register</button>
-      </NavLink>
-      <NavLink to='/login'>
-        <button className='login'>Login</button>
-      </NavLink>
-    </div>
-  </div>
+		<div className='unauth'>
+			<NavLink to='/landing' style={style}>
+				<h1>Library</h1>
+			</NavLink>
+			<div className='auth-buttons'>
+				<NavLink to='/register'>
+					<button className='register'>Register</button>
+				</NavLink>
+				<NavLink to='/login'>
+					<button className='login'>Login</button>
+				</NavLink>
+			</div>
+		</div>
 	) : (
-  <div className='auth'>
-    <NavLink to='/books' style={style}>
-      <h1>Library</h1>
-    </NavLink>
-    <div className='search'>
-      <input type='text' placeholder='Search a book...' />
-      <button>Search</button>
-    </div>
-    <div className='auth-header-links'>
-      <NavLink to='/books' className='books-header'>
-        Books
-      </NavLink>
-      <NavLink to='/user' className='user-header'>
-        Profile
-      </NavLink>
-      <button className='logout' onClick={logout}>
-        Logout
-      </button>
-    </div>
-  </div>
+		<div className='auth'>
+			<NavLink to='/books' style={style}>
+				<h1>Library</h1>
+			</NavLink>
+			<div className='search'>
+				<input type='text' placeholder='Search a book...' />
+				<button>Search</button>
+			</div>
+			<div className='auth-header-links'>
+				<NavLink to='/books' className='books-header'>
+					Books
+				</NavLink>
+				<NavLink to='/user' className='user-header'>
+					Profile
+				</NavLink>
+				<button className='logout' onClick={logout}>
+					Logout
+				</button>
+			</div>
+		</div>
 	);
 
 	return <header className='header'>{header_option}</header>;
--- client/library/src/components/RegisterPage/RegisterPage.jsx
+++ client/library/src/components/RegisterPage/RegisterPage.jsx
@@ -1,60 +1,117 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import { useHistory } from 'react-router-dom';
-import userData from './../../data/userData'
+import userData from './../../data/userData';
+import {toastSuccess, toastError} from './../../common/toaster'
+
 
 const RegisterPage = () => {
-	const [user, setUser] = useState('');
-	const [pass, setPass] = useState('');
-
-	const data = { username: user, password: pass };
 
   let history = useHistory();
+  const initialForm = {
+    username: {
+      value: '',
+      name: 'username',
+      type: 'text',
+      placeholder: 'Username...',
+      valid: true,
+      validators: {
+        required: true,
+        minLength: 1,
+        maxLength: 20,
+      }
+    },
+    password: {
+      value: '',
+      name: 'password',
+      type: 'password',
+      placeholder: 'Password...',
+      valid: true,
+      validators: {
+        required: true,
+        minLength: 1,
+        maxLength: 20,
+      }
+    }
+  }
+
+  const [form, setForm] = useState(initialForm);
+
+  const onChange = (ev) => {
+    const { name, value } = ev.target;
+
+    const currentTarget = { ...form[name] };
+    currentTarget.value = value;
+    currentTarget.valid = true;
+
+    if (currentTarget.validators.required) {
+      currentTarget.valid = currentTarget.valid && currentTarget.value.length > 0;
+    }
+
+    if (currentTarget.validators.minLength) {
+      currentTarget.valid = currentTarget.valid && currentTarget.value.length >= currentTarget.validators.minLength;
+    }
+
+    if (currentTarget.validators.maxLength) {
+      currentTarget.valid = currentTarget.valid && currentTarget.value.length <= currentTarget.validators.maxLength;
+    }
+
+    if (!currentTarget.validators.required && !currentTarget.value.length) {
+      currentTarget.valid = true;
+    }
+
+    setForm({ ...form, [name]: currentTarget });
+  }
+  
+	const formView = Object.values(form).map((input) => {
+    return (
+      <Fragment key={input.name}>
+        <input
+          style={
+            input.valid
+              ? { border: '1px solid grey' }
+              : { border: '1px solid red' }
+          }
+          name={input.name}
+          type={input.type}
+          placeholder={input.placeholder}
+          value={input.value}
+          onChange={onChange}
+        />
+        <br />
+      </Fragment>
+		);
+	});
 
 
-	const registerUser = () => {
-    userData.registerUser(data)
+	const registerUser = (ev) => {
+    ev.preventDefault()
+
+    const registerData = Object.values(form).reduce((data, input) => {
+      return { ...data, [input.name]: input.value };
+    }, {});
+
+    userData.registerUser(registerData)
 			.then((res) => {
         if(res.data.user) {
-          alert(`${res.data.message} Click OK to be redirected to login`)
+          toastSuccess(`${res.data.message} You are redirected to login`)
           history.push('/login')
         } else {
           throw new Error (res.data.message)
         }
 			})
-      .catch((err) => alert(err))
+      .catch((err) => toastError(err.response.data.message))
 	};
 
-	const updateUser = (value) => setUser(value);
-	const updatePass = (value) => setPass(value);
-
 	return (
-  <div className='login-page-layout'>
-    <div className='call-to-action'>
-      <h1 className='call-to-action-text'>
-        Register to get access to the world's greatest stories
-      </h1>
+  <div>
+    <h1 className='call-to-action-text'>
+      Don't be stupid. Register now.
+    </h1>
+    <form className="register-form" onSubmit={registerUser}>
+      {formView}
       <br />
-      <span>User</span>
-      <input
-        placeholder='username..'
-        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-        onChange={(e) => updateUser(e.target.value)}
-        value={user}
-      />
-      <br />
-      <span>Pass</span>
-      <input
-        type='password'
-        placeholder='password..'
-        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-        onChange={(e) => updatePass(e.target.value)}
-        value={pass}
-      />
-      <br />
-      <button className='register-button' onClick={registerUser}>
-        Register
-      </button>
-    </div>
+      <button type="submit">Register</button>
+    </form>
   </div>
 	);
 };
