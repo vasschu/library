799ac33d8c36dee0c9d50c00c78586dbe3e613f6
Virzhinia Petkova <petkovavirzhinia@gmail.com>
2020-11-01 16:14:31 +0200

    fixed wring import

--- client/library/src/hooks/useBorrowBook.js
+++
@@ -1,5 +0,0 @@
-import BooksService from '../data/booksData.js'
-
-export const useBorrowBook = (id) => {
-    return BooksService.borrowBook(id)
-}
--- client/library/src/index.js
+++ client/library/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'reactstrap/dist/reactstrap.min'
 import './index.css';
 
 ReactDOM.render(
