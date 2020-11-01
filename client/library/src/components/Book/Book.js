e8dca41273a40c1b3b839c1ec422f69477f19db2
Vassil Christov <vassil.christov@gmail.com>
2020-11-01 23:09:52 +0200

    book covers are now clickable

--- client/library/src/components/Book/Book.js
+++ client/library/src/components/Book/Book.js
@@ -1,28 +1,34 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import {NavLink} from 'react-router-dom';
-import BorrowButton from './BorrowButton'
-import './Book.css'
+import { NavLink, Link } from 'react-router-dom';
+import BorrowButton from './BorrowButton';
+import './Book.css';
 
-const Book = ({book, borrowBook, returnBook, tokenData}) => {
+const Book = ({ book, borrowBook, returnBook, tokenData }) => {
+	const { id, image, title, author, borrowed, borrow_user } = book;
 
-    const {id, image, title, author, borrowed, borrow_user } = book;
-  
-return (
-  <div className="book">
-    <img src={image} alt="book-cover" />
-    <div className="book-info">
-      <h2>{title}</h2>
-      <p className="author">{author}</p>
-      <NavLink to={'/books/'+ id}><button className="book-details-btn">View Details</button></NavLink>
-      <br />
-      <BorrowButton
-        borrowed={borrowed} logedUser={tokenData.sub} borrowUser={borrow_user} 
-        borrowBook={() => borrowBook(id)} returnBook={() => returnBook(id)}
-      />
-    </div>
-  </div>
-    )
-}
+	return (
+		<div className='book'>
+			<Link to={'/books/' + id}>
+				<img src={image} alt='book-cover' />
+			</Link>
+			<div className='book-info'>
+				<h2>{title}</h2>
+				<p className='author'>{author}</p>
+				<NavLink to={'/books/' + id}>
+					<button className='book-details-btn'>View Details</button>
+				</NavLink>
+				<br />
+				<BorrowButton
+					borrowed={borrowed}
+					logedUser={tokenData.sub}
+					borrowUser={borrow_user}
+					borrowBook={() => borrowBook(id)}
+					returnBook={() => returnBook(id)}
+				/>
+			</div>
+		</div>
+	);
+};
 
 export default Book;
